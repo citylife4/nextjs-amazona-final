240f62d4d113ad830abd8a9ea7386b8f1b57b3f0
Basir <basir.jafarzadeh@gmail.com>
2021-04-19 12:17:07 +0430

    update env for MONGODB_URI

--- utils/db.js
+++ utils/db.js
@@ -16,7 +16,7 @@ async function connect() {
     }
     await mongoose.disconnect();
   }
-  const db = await mongoose.connect(process.env.MONGODB_URL, {
+  const db = await mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
